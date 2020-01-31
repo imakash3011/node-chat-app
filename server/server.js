ec7264bbc1737631962192e1dacd212c41356566
imakash3011 <imakashpatel3011@gmail.com>
2020-02-01 00:14:48 +0530

    removed the node modules file

--- server/server.js
+++ server/server.js
@@ -11,11 +11,12 @@ const express =require('express');
 
 const publicPath =path.join(__dirname, '../public')
 // console.log(publicPath);
+const port = process.env.PORT || 3000;
 var app =express();
 
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log(`Server is up on ${port}`)
 })
