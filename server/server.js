e3746e5c83527e27539c2c494b34088d54e3bd1c
imakash3011 <imakashpatel3011@gmail.com>
2020-02-28 22:11:41 +0530

    added connected and disconnected feature

--- server/server.js
+++ server/server.js
@@ -6,17 +6,32 @@ console.log(__dirname + '/../public');
 
 //check the path module documentation from nodejs.org
 const path =require('path')
+const http =require('http');
 const express =require('express');
+const socketIO= require('socket.io')
 
 
 const publicPath =path.join(__dirname, '../public')
 // console.log(publicPath);
 const port = process.env.PORT || 3000;
 var app =express();
+var server = http.createServer(app);
+var io =socketIO(server);
 
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+//popular event...it help to listen for new connection
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+
+    //when the user leaves/disconnect the chat
+    socket.on('disconnect',()=>{
+        console.log('User was Disconnected');
+    });
+})
+
+
+server.listen(port,()=>{
     console.log(`Server is up on ${port}`)
 })
