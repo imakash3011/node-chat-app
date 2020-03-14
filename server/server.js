e1bd9c0f15fc603db73b14ba4b5d75440aa7ad74
imakash3011 <imakashpatel3011@gmail.com>
2020-03-15 00:32:27 +0530

    added username and room name in chat app

--- server/server.js
+++ server/server.js
@@ -41,17 +41,27 @@ io.on('connection', (socket)=>{
 
     // });
 
-    //socket.emit from Admin text welcome to the chat app
-    socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
-
-
-    // socket.broadcast.emit from Admin text new user joined
-    socket.broadcast.emit('newMessage',generateMessage('Admin','New User Joined'));
-
+    
     socket.on('join',(params,callback)=>{
         if (!isRealString(params.name) || !isRealString(params.room)){
             callback('Name and room name are required. ');
         }
+
+        // to join the room ..with the same string name
+        socket.join(params.room);
+        //socket.leave('') ..to leave the chat room 
+
+        //io.emit -> io.to('The Office Fans').emit  will send msg to everyone connected to 'The Office Fans'
+        //socket.broadcast.emit -> io.to('The Office Fans').emit will send msg to everyone except the current user
+        //socket.emit
+
+        //socket.emit from Admin text welcome to the chat app
+        socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
+
+
+        // socket.broadcast.emit from Admin text new user joined who joined the room
+        socket.broadcast.to(params.room).emit('newMessage',generateMessage('Admin',`${params.name} has joined.`));
+
         callback();
     });
 
